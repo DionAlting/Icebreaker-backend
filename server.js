ecf54f294770ed660886d8d16991f2716a5a6831
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-04-20 14:13:59 +0200

    socketio setup

--- server.js
+++ server.js
@@ -3,13 +3,38 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
 const app = express();
-const PORT = 4000;
+const http = require("http");
+const socketIo = require("socket.io");
 
 app.use(helmet());
 app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
+const server = http.createServer(app);
+const options = {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+};
+
+const io = socketIo(server, options);
+
+function onConnect(socket) {
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+}
+
+io.on("connection", onConnect);
+
+const port = 4001;
+
+function onListen() {
+  console.log(`Listening on ${port}`);
+}
+
+// Start the app
+server.listen(port, onListen);
